ce93d6abcd127de58f10e20d3c8293c5b0c9347e
alberto.leon.crespo@gmail.com <alberto.leon.crespo@gmail.com>
2023-01-13 14:57:45 +0100

    Added repository and query to feeds module

--- src/modules/feeds/feeds.module.ts
+++ src/modules/feeds/feeds.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FeedController } from './infrastructure/controllers/feed.controller';
+import FeedRepositoryMongo from './infrastructure/adapters/repository/feed.repository.mongo';
+import { GetAllFeedsQuery } from './application/querys/get-all-feeds.query';
 
 @Module({
   imports: [],
   controllers: [FeedController],
-  providers: [],
+  providers: [FeedRepositoryMongo],
+  exports: [FeedRepositoryMongo, GetAllFeedsQuery],
 })
 export class FeedsModule {}
