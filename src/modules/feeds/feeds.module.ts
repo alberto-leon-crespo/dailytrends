27bbba62912abdba9886f8a6e68814f948181b62
Alberto León Crespo <alberto.leon.crespo@gmail.com>
2023-01-14 22:39:47 +0100

    Added getFeedById api call

--- src/modules/feeds/feeds.module.ts
+++ src/modules/feeds/feeds.module.ts
@@ -7,13 +7,19 @@ import {
 } from './infrastructure/adapters/schema/feed.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GetAllFeedsQuery } from './application/querys/get-all-feeds.query';
+import { GetFeedByIdQuery } from './application/querys/get-feed-by-id.query';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: FeedModel.name, schema: FeedSchema }]),
   ],
   controllers: [FeedController],
-  providers: [FeedModel, FeedRepositoryMongo, GetAllFeedsQuery],
+  providers: [
+    FeedModel,
+    FeedRepositoryMongo,
+    GetAllFeedsQuery,
+    GetFeedByIdQuery,
+  ],
   exports: [FeedRepositoryMongo, MongooseModule],
 })
 export class FeedsModule {}
--- src/modules/feeds/infrastructure/controllers/feed.controller.ts
+++ src/modules/feeds/infrastructure/controllers/feed.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
 import { GetAllFeedsQuery } from '../../application/querys/get-all-feeds.query';
+import { GetFeedByIdQuery } from '../../application/querys/get-feed-by-id.query';
 
 @Controller('feeds')
 export class FeedController {
-  public constructor(private getAllFeedsUsecase: GetAllFeedsQuery) {}
+  public constructor(
+    private getAllFeedsUsecase: GetAllFeedsQuery,
+    private getFeedByIdQuery: GetFeedByIdQuery,
+  ) {}
   @Get()
   public async getFeeds(@Res() request): Promise<any> {
     const feeds = await this.getAllFeedsUsecase.run();
     return request.status(HttpStatus.OK).json(feeds);
+  }
+
+  @Get('/:id')
+  public async getFeedById(
+    @Res() request,
+    @Param('id') id: string,
+  ): Promise<any> {
+    const feed = await this.getFeedByIdQuery.run(id);
+    return request.status(HttpStatus.OK).json(feed);
   }
 }
